cc7b93d20f944fc6294c2183e7882e707e114db8
maria20174 <mbuzmakova0@gmail.com>
2026-07-13 16:24:23 +0300

    Create VersionAttribute.cs

---
+++ task07/VersionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace task07;
+
+[AttributeUsage(AttributeTargets.Class,Inherited = true, AllowMultiple = false)]
+public class VersionAttribute: Attribute
+{
+    public int Major {get;}
+    public int Minor {get;}
+    public VersionAttribute(int major, int minor)
+    {
+        Major = major;
+        Minor = minor;
+    }
+}
