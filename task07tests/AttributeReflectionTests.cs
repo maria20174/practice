25eb4dfb292abf4a351b1c58f79d45aa35af9052
maria20174 <mbuzmakova0@gmail.com>
2026-07-13 16:30:42 +0300

    Create AttributeReflectionTests.cs

---
+++ task07tests/AttributeReflectionTests.cs
@@ -0,0 +1,58 @@
+using System.Reflection;
+using task07;
+namespace task07tests;
+
+public class AttributeReflectionTests
+{
+    [Fact]
+    public void Class_HasDisplayNameAttribute()
+    {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Пример класса", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Method_HasDisplayNameAttribute()
+    {
+        var method = typeof(SampleClass).GetMethod("TestMethod");
+        var attribute = method.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Тестовый метод", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Property_HasDisplayNameAttribute()
+    {
+        var prop = typeof(SampleClass).GetProperty("Number");
+        var attribute = prop.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Числовое свойство", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Class_HasVersionAttribute()
+    {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<VersionAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal(1, attribute.Major);
+        Assert.Equal(0, attribute.Minor);
+    }
+
+    [Fact]
+    public void Class_PrintTypeInfo()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        ReflectionHelper.PrintTypeInfo(typeof(SampleClass));
+        Assert.Contains("SampleClass", output.ToString());
+        Assert.Contains("Пример класса", output.ToString());
+        Assert.Contains("Version: 1.0", output.ToString());
+        Assert.Contains("TestMethod", output.ToString());
+        Assert.Contains("Тестовый метод", output.ToString());
+        Assert.Contains("Number", output.ToString());
+        Assert.Contains("Числовое свойство", output.ToString());
+    }
+}
